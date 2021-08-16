ac38e28dd24e5b96c88bda39e2e0f4dbe898711c
xuhaitong <2452009536@qq.com>
2021-08-16 04:42:04 -0700

    lab2 done

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -1,4 +1,5 @@
 #include "tcp_receiver.hh"
+#include <iostream>
 
 // Dummy implementation of a TCP receiver
 
@@ -10,10 +11,27 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+void TCPReceiver::segment_received(const TCPSegment& seg) {
+    if (!_isn.has_value()) {
+        if (!seg.header().syn) {
+            std::cerr << "Error: connection not build" << std::endl;
+            return;
+        }
+        _isn = seg.header().seqno + 1;
+    }
+    string&& payload = string(seg.payload().str());
+    uint64_t index = unwrap(seg.header().seqno + (seg.header().syn ? 1 : 0), _isn.value(), _reassembler.expect());
+    bool eof = seg.header().fin;
+    _reassembler.push_substring(payload, index, eof);
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const {
+    if (_isn.has_value()) {
+        return { wrap(_reassembler.expect(), _isn.value()) + (_reassembler.stream_out().input_ended() ? 1 : 0) };
+    }
+    return std::nullopt;
+}
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const {
+    return _capacity - _reassembler.stream_out().buffer_size();
+}
